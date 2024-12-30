128330125c9618899bbb1860c2f89db38f56cb34
dilanozer <dozer31415@gmail.com>
2024-12-30 21:01:00 +0300

    Sorting, Filtering, Searching & Pagination

---
+++ API/RequestHelpers/Pagination.cs
@@ -0,0 +1,9 @@
+namespace API.RequestHelpers;
+
+public class Pagination<T>(int pageIndex, int PageSize, int count, IReadOnlyList<T> data)
+{
+    public int PageIndex { get; set; } = pageIndex;
+    public int PageSize { get; set; } = PageSize;
+    public int Count { get; set; } = count;
+    public IReadOnlyList<T> Data { get; set; } = data;
+}
