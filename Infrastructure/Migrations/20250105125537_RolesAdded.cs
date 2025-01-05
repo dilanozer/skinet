08ee4c948c78a3e11a4bf6b9a9b553178054f607
dilanozer <dozer31415@gmail.com>
2025-01-05 18:12:40 +0300

    Role based authorization

---
+++ Infrastructure/Migrations/20250105125537_RolesAdded.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RolesAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "aa49dddd-1e90-4785-afcf-16c12de372c3", null, "Admin", "ADMIN" },
+                    { "e7b2165a-f8c4-4031-bb44-3db9f09d7cc7", null, "Customer", "CUSTOMER" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "aa49dddd-1e90-4785-afcf-16c12de372c3");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "e7b2165a-f8c4-4031-bb44-3db9f09d7cc7");
+        }
+    }
+}
