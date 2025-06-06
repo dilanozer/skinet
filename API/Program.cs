7bc09fdb996799979d620d2b61d7613d0462d889
dilanozer <dozer31415@gmail.com>
2025-06-06 23:44:23 +0300

    API skeleton

---
+++ API/Program.cs
@@ -0,0 +1,20 @@
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<StoreContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.MapControllers();
+
+app.Run();
