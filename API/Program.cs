d33534c2081248b0d76eadf1215aa82564898316
dilanozer <dozer31415@gmail.com>
2025-06-20 12:20:25 +0300

    Client - Identity

--- API/Program.cs
+++ API/Program.cs
@@ -37,6 +37,8 @@ var app = builder.Build();
 app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowCredentials()
     .WithOrigins("http://localhost:4200", "https://localhost:4200"));
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapControllers();
 app.MapGroup("api").MapIdentityApi<AppUser>(); // api/login  api/register ...
 
